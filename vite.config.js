1702dc5d0117801466c99c1e6275da3d604ed3d4
ishimwegloire01 <ishimwegloire01@gmail.com>
2026-05-26 18:17:53 +0200

    Init: Vite + React and Tailwindcss Setup

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   resolve: {
+//   alias: {
+//     '@': path.resolve(__dirname, './src'),
+//   },
+// },
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Reconstruct __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
